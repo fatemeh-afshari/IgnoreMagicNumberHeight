1524d75bc548fdf6813a7aa89b5446dc604c7ea3
Fatemeh Afshari <67331684+fatemeh-afshari@users.noreply.github.com>
2024-09-25 20:07:10 +0330

    Update MyRuleSetProvider.kt

--- src/main/kotlin/org/example/detekt/MyRuleSetProvider.kt
+++ src/main/kotlin/org/example/detekt/MyRuleSetProvider.kt
@@ -11,7 +11,7 @@ class MyRuleSetProvider : RuleSetProvider {
         return RuleSet(
             ruleSetId,
             listOf(
-                MyRule(config),
+                IgnoreHeightMagicNumber(config),
             ),
         )
     }
